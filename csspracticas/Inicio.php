9bd17d78f0bb49025a541babf4035c429d7c87ca
Israel Tores <nefta_2_7@hotmail.com>
2017-05-16 10:16:47 -0500

    Contenidos

---
+++ csspracticas/Inicio.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <!--<link rel="shortcut icon" href="../imagenes/manza.ico">-->
+        <link href="cssinicio/inicio.css" rel="stylesheet" type="text/css"/>
+        <title>Default</title>
+    </head> 
+    <body>       
+        <div class="contenedor">
+            <div class="subcontenido">
+                SOMBRAS
+            </div>
+        </div>
+    </body>
+</html>
