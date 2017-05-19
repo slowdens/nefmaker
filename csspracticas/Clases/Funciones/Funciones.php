bcd2e5182d4bf80aa5e62cf849d128b63af75a2d
Israel Tores <nefta_2_7@hotmail.com>
2017-05-19 10:46:00 -0500

    sacar bob

--- csspracticas/Clases/Funciones/Funciones.php
+++ csspracticas/Clases/Funciones/Funciones.php
@@ -28,23 +28,37 @@ class Funciones {
        }
        
        public function InformacionGeneral(){
-           require_once '../Conexiones.php';           
-           /*Creamos la instancia del objeto. Ya estamos conectados*/
-           $bd=  Conexiones::getInstance();
-           
-           $sql='CALL sp_informacion;';
-           /*Ejecutamos la query*/
-           $stmt = $bd->ejecutar($sql);
-           $this-> rawdata = array(); //creamos un array
-           $i=0;
-           /*Realizamos un bucle para ir obteniendo los resultados*/
-           $jsons= "[";
-           while ($x=$bd->obtener_fila($stmt,0)){
-              $jsons .='{"Fruta": "'.$x[0].'" },';  //$x[0];
-
-                    $i++;
-           }
-           return $this->json;
+        require_once '../ConServidor.php';
+        $base = new ConServidor();        
+        $datos = array();
+        $sql='CALL sp_informacion;';        
+        $mysqli = new mysqli($base->getServidor(),$base->getUsuario(), $base->getPassword(), $base->getBasedeDatos());
+        /* comprobar la conexión */
+        if ($mysqli->connect_errno) {
+            printf("Falló la conexión: %s\n", $mysqli->connect_error);
+            exit();
+                        /* Si se ha de recuperar una gran cantidad de datos se emplea MYSQLI_USE_RESULT */
+        }
+        if ($resultado = $mysqli->query($sql, MYSQLI_USE_RESULT)) {                
+            $i=0;
+            while($obj = $resultado->fetch_object()){
+                  $datos[$i] =array('Pantalla'=>$obj->Pantalla,'Link'=>$obj->Link,'Icono'=>$obj->icono) ;
+                  $id=$obj->id;                
+                  
+                  
+                  $Titulo = $obj->Titulo;
+                  $DescripcionHtml = $obj->DescripcionHtml;
+                  $i++;
+                    
+            } 
+                /* Observar que no se puede ejecutar ninguna función que interactue con el
+                   servidor hasta que el conjunto de resultados se haya cerrado. Todas las llamadas devolverán un
+                   error 'out of sync' */
+                
+            $resultado->close();
+        }
+        $mysqli->close();
+        return $datos;
        }
                
        
